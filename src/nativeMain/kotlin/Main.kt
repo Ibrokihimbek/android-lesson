9572174d02091402be25ca22dec71b835cc2cb6d
Ibrohimbek Davronbekov <108320639+Ibrokihimbek@users.noreply.github.com>
2023-07-24 09:34:03 +0500

    first lesson

---
+++ src/nativeMain/kotlin/Main.kt
@@ -0,0 +1,32 @@
+fun main() {
+//    val oneMillion = 50
+//    if (oneMillion in 1..100){
+//        println(oneMillion)
+//    }else{
+//        println("Kallanga")
+//    }
+
+
+//    var b: String = ""
+//    for (i in 1..10) {
+//        if (i % 2 == 0) {
+//            b += " "
+//        } else {
+//            b += i
+//        }
+//
+//    }
+//    println(b)
+
+
+//    repeat(4){
+//        println("Hello Kotlin")
+//    }
+
+//    val myList = mutableListOf<Int>()
+//    myList += 4
+//    println(myList)
+
+    val listReversed = "HELLO"
+    println(listReversed.toLowerCase())
+}
